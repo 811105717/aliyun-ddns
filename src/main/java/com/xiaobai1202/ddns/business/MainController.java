6a3445df4d99abd0e437a4519233d3db5e10edb5
jinfeng.bai <baijinfeng1202@126.com>
2020-12-19 11:13:41 +0800

    T-000 Update gitignore.

--- src/main/java/com/xiaobai1202/ddns/business/MainController.java
+++ src/main/java/com/xiaobai1202/ddns/business/MainController.java
@@ -1,6 +1,5 @@
 package com.xiaobai1202.ddns.business;
 
-import com.aliyuncs.IAcsClient;
 import com.aliyuncs.exceptions.ClientException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,21 +7,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
 public class MainController {
 
     @Autowired
-    private IAcsClient client;
-
-    @Autowired
     private MainService mainService;
 
     @RequestMapping("/")
-    public ResponseEntity<String> indedx() throws ClientException, IOException {
+    public void index(HttpServletResponse response) throws ClientException, IOException {
         String log = mainService.logs();
-        return new ResponseEntity(log, HttpStatus.OK);
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType("text/html;charset=utf8");
+        response.setContentLength(log.length());
+        response.getWriter().println(log);
+        response.flushBuffer();
     }
 
     @RequestMapping("/update")
